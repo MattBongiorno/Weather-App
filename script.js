fbd3e9c2e7a70e25542eb8c523a6adf8f829a41d
Matt Bongiorno <bigiorno@gmail.com>
2020-08-08 10:26:05 -0400

    API call

--- script.js
+++ script.js
@@ -7,3 +7,52 @@ const displayList = JSON.parse(localStorage.getItem("displayList")) || [{ town: 
 let zipCode = displayList[0].zip;
 const recentSearches = document.getElementById('recentSearches');
 const searchBtn = document.getElementById("search-btn");
+
+zipCodeSearch.addEventListener("keyup", () => {
+    searchBtn.disabled = !zipCodeSearch.value
+});
+
+
+
+function firstAjaxCall() {
+    $.ajax({
+        url: (`https://api.openweathermap.org/data/2.5/weather?zip=${zipCode},us&appid=${APIKey}`),
+        method: "GET"
+    })
+
+    .then(function(response) {
+        $(".cityCurrent").html(response.name + moment().format(' MMMM Do YYYY')); //add city and date
+
+        const latitude = (response.coord.lat);
+        const longitude = (response.coord.lon);
+        const location = {
+            latitude: latitude,
+            longitude: longitude
+        }
+        secondAjaxCall(location);
+        townName = response.name;
+        saveRecentSearch();
+
+    });
+}
+firstAjaxCall();
+
+searchBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    dayContainer.empty();
+    zipCode = zipCodeSearch.value
+    firstAjaxCall();
+    zipCodeSearch.value = ""
+});
+
+function saveRecentSearch() {
+    const recentSearch = {
+        town: townName,
+        zip: zipCode
+    }
+
+    displayList.unshift(recentSearch);
+    displayList.splice(5);
+    localStorage.setItem("displayList", JSON.stringify(displayList));
+    printRecentSearch();
+};
